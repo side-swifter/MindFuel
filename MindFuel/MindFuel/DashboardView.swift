0400d6b0a668b68f3445a25a4f71f7d3f1ba7a21
side-swifter <sideswifter2010@gmail.com>
2025-08-04 10:52:52 -0400

    Fix UIColor scope issues - add UIKit imports
    - Added UIKit imports to DashboardView.swift, Theme.swift, and AlertsView.swift
    - Fixed all 'Cannot find UIColor in scope' errors
    - Build now succeeds completely without any errors
    - App is ready to run in Xcode

--- MindFuel/MindFuel/DashboardView.swift
+++ MindFuel/MindFuel/DashboardView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import SwiftData
+import UIKit
 
 struct DashboardView: View {
     @Environment(\.modelContext) private var modelContext
--- MindFuel/MindFuel/Theme.swift
+++ MindFuel/MindFuel/Theme.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import UIKit
 
 // MARK: - Brand Colors
 extension Color {
